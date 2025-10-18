e4f655d772aec36b6fe8cfc87d5ec1fe34060ce8
shehharyar28 <sheharyar1456@gmail.com>
2025-10-19 01:51:37 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_size_matters/flutter_size_matters.dart';
 import 'package:icare/app.dart';
-import 'package:icare/screens/splash.dart';
-import 'package:icare/screens/walkthrough.dart';
 import 'package:icare/utils/theme.dart';
 
 void main() {
@@ -12,7 +9,7 @@ void main() {
     ProviderScope(child: const MyApp())
     );
 }
-
+// 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
